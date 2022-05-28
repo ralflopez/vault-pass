d6d73666590babdc64830e4943a8d86f88cad49e
Ralf <ralflopez3@gmail.com>
2022-05-28 08:03:07 +0800

    :test_tube: write failing test for find one vault invalid and invalid input

--- src/server/test/integration/vault.integration.spec.ts
+++ src/server/test/integration/vault.integration.spec.ts
@@ -3,22 +3,56 @@ import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../../app.module';
 import { PrismaService } from '../../prisma/prisma.service';
-import { vaultStubForUser1, vaultStubForUser2 } from '../stubs/vault.stub';
+import { vaultStub } from '../stubs/vault.stub';
 import { CreateVaultDto } from '../../vaults/dto/create-vault.dto';
-import { decrypt } from '../../vaults/helpers/encryption';
+import {
+  decrypt,
+  generateAuthHash,
+  generateEncryptionKey,
+} from '../../vaults/helpers/encryption';
 
 describe('VaultController (e2e)', () => {
   let app: INestApplication;
   let prisma: PrismaService;
 
+  const user1 = {
+    encryptionKey: generateEncryptionKey('demo@email.com', 'password'),
+    authHash: generateAuthHash('demo@email.com', 'password'),
+  };
+
+  const user2 = {
+    encryptionKey: generateEncryptionKey('demo2@email.com', 'password'),
+    authHash: generateAuthHash('demo2@email.com', 'password'),
+  };
+
   function seed() {
     // seed
     prisma.vault.createMany({
       data: [
-        vaultStubForUser1('amazon.com', 'amazonPassword'),
-        vaultStubForUser1('google.com', 'googlePassword'),
-        vaultStubForUser2('facebook.com', 'facebookPassword'),
-        vaultStubForUser2('twitter.com', 'twitterPassword'),
+        vaultStub(
+          user1.encryptionKey,
+          user1.authHash,
+          'amazon.com',
+          'password1',
+        ),
+        vaultStub(
+          user1.encryptionKey,
+          user1.authHash,
+          'facebook.com',
+          'password1',
+        ),
+        vaultStub(
+          user2.encryptionKey,
+          user2.authHash,
+          'twitter.com',
+          'password2',
+        ),
+        vaultStub(
+          user2.encryptionKey,
+          user2.authHash,
+          'amazon.com',
+          'password2',
+        ),
       ],
     });
   }
@@ -73,6 +107,23 @@ describe('VaultController (e2e)', () => {
         } as CreateVaultDto);
 
       expect(res.status).toBe(HttpStatus.BAD_REQUEST);
+    });
+  });
+
+  describe('findOneVault', () => {
+    it('should find a vault with all records for a user', async () => {
+      seed();
+      const res = await request(app.getHttpServer()).get(
+        `/vaults/${user1.authHash}`,
+      );
+      expect(res.body.length).toBe(2);
+    });
+
+    it('should return a FORBIDDEN error if no vault found', async () => {
+      const res = await request(app.getHttpServer()).get(
+        `/vaults/${generateAuthHash('invalid@email.com', 'password')}`,
+      );
+      expect(res.status).toBe(HttpStatus.FORBIDDEN);
     });
   });
 });
--- src/server/test/stubs/vault.stub.ts
+++ src/server/test/stubs/vault.stub.ts
@@ -1,25 +1,13 @@
 import { Vault } from '@prisma/client';
-import {
-  encrypt,
-  generateAuthHash,
-  generateEncryptionKey,
-} from '../../vaults/helpers/encryption';
+import { encrypt } from '../../vaults/helpers/encryption';
 import * as crypto from 'crypto';
 
-export const vaultStubForUser1 = (domain: string, password: string): Vault => {
-  const encryptionKey = generateEncryptionKey('demo@email.com', 'password');
-  const authHash = generateAuthHash('demo@email.com', 'password');
-  return {
-    id: crypto.randomBytes(16).toString('hex'),
-    authHash,
-    domain,
-    value: encrypt(password, Buffer.from(encryptionKey, 'hex')),
-  };
-};
-
-export const vaultStubForUser2 = (domain: string, password: string): Vault => {
-  const encryptionKey = generateEncryptionKey('demo2@email.com', 'password');
-  const authHash = generateAuthHash('demo2@email.com', 'password');
+export const vaultStub = (
+  encryptionKey: string,
+  authHash: string,
+  domain: string,
+  password: string,
+): Vault => {
   return {
     id: crypto.randomBytes(16).toString('hex'),
     authHash,

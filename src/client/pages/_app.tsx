7913c6b248d51fa142fa3fe60d7608203b02915a
Ralf <ralflopez3@gmail.com>
2022-05-28 17:17:04 +0800

    :heavy_plus_sign: install chakra ui

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 import favicon from '../public/favicon.ico';
-
+import { ChakraProvider } from '@chakra-ui/react';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -10,7 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" type="image/x-icon" href={favicon.src} />
         <title>VaultPass</title>
       </Head>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   );
 }
